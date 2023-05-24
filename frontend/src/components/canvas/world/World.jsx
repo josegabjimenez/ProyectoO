5442ba8bbce690f3d8d2b3124f7902cb075db625
josegabjimenez <josegabojimenez@gmail.com>
2023-05-24 17:22:41 -0500

    working on deployment fix

--- frontend/src/components/canvas/world/World.jsx
+++ frontend/src/components/canvas/world/World.jsx
@@ -1,30 +1,28 @@
 'use client'
 
-import React, { useRef, Suspense } from "react";
-import { useGLTF, Sky } from "@react-three/drei";
+import React, { useRef, Suspense } from 'react'
+import { useGLTF, Sky } from '@react-three/drei'
 // import {Perf} from 'r3f-perf';
 
-import Water from "./Water";
+import Water from './Water'
 // import { GLTFLoader } from "three-stdlib";
 
-
 export function ModelWorld(props) {
-  
-  const { scene } = useGLTF('/models/world/world.glb')
+  const { scene } = useGLTF('/models/world/coliseo.glb')
   // const { nodes, materials } = useGLTF("/models/world/world.glb");
-  return (<>
-  {/* <Perf position="top-left" /> */}
-    <Sky sunPosition={[7, 60, 1]} rayleigh={0.4}/>
-    <Suspense fallback={null}>
-    <primitive object={scene} {...props} />
-    </Suspense>
-    <Water/>
+  return (
+    <>
+      {/* <Perf position="top-left" /> */}
+      <Sky sunPosition={[7, 60, 1]} rayleigh={0.4} />
+      <Suspense fallback={null}>
+        <primitive object={scene} {...props} />
+      </Suspense>
+      <Water />
     </>
-  );
+  )
 }
 
-useGLTF.preload("/models/world/world.glb");
-
+useGLTF.preload('/models/world/coliseo.glb')
 
 export function Dog(props) {
   const { scene } = useGLTF('/dog.glb')
